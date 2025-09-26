2955ada7a8d9ba6bae16d26386e3609f57380a15
abhijith_g1 <abhijith_g@geojit.com>
2025-09-26 17:56:21 +0530

    Implemented Mim=nimum coins problem using memoziation

--- minimumcoins.cpp
+++ minimumcoins.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int compute(int index,int target,vector<int>&arr){
+    int compute(int index,int target,vector<int>&arr,vector<vector<int>>&dp){
 
         if(index==0){
             if(target%arr[index]==0) return target/arr[index];
             else return 1e9;
         }
-        int nonTake = compute(index-1,target,arr);
+        if(dp[index][target]!=-1) return dp[index][target];
+        int nonTake = compute(index-1,target,arr,dp);
         int take = INT_MAX;
-        if(arr[index]<=target) take=1+compute(index,target-arr[index],arr);
-        return min(nonTake,take);
+        if(arr[index]<=target) take=1+compute(index,target-arr[index],arr,dp);
+        dp[index][target] = min(nonTake,take);
+        return dp[index][target];
 
     }
     int coinChange(vector<int>& coins, int amount) {
-        int ans= compute(coins.size()-1,amount,coins);
+        int index = coins.size();
+        vector<vector<int>> dp(index, vector<int>(amount + 1, -1));
+        int ans= compute(index-1,amount,coins,dp);
         if(ans>=1e9) return -1;
         return ans;
     }
