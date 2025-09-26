d395a0314d1be0e01fcba51f2b2e57efaef4e729
abhijith_g1 <abhijith_g@geojit.com>
2025-09-26 17:56:55 +0530

    Implemented Mim=nimum coins problem using tabulation

--- minimumcoins.cpp
+++ minimumcoins.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int compute(int index,int target,vector<int>&arr,vector<vector<int>>&dp){
-
-        if(index==0){
-            if(target%arr[index]==0) return target/arr[index];
-            else return 1e9;
-        }
-        if(dp[index][target]!=-1) return dp[index][target];
-        int nonTake = compute(index-1,target,arr,dp);
-        int take = INT_MAX;
-        if(arr[index]<=target) take=1+compute(index,target-arr[index],arr,dp);
-        dp[index][target] = min(nonTake,take);
-        return dp[index][target];
-
-    }
     int coinChange(vector<int>& coins, int amount) {
         int index = coins.size();
-        vector<vector<int>> dp(index, vector<int>(amount + 1, -1));
-        int ans= compute(index-1,amount,coins,dp);
+        vector<vector<int>> dp(index, vector<int>(amount + 1, 1e9));
+        for(int k=0;k<=amount;k++) {
+            if(k%coins[0]==0) dp[0][k] = k/coins[0];
+        }
+        for(int i=1;i<index;i++){
+            for(int j=0;j<=amount;j++){
+                int nonTake = dp[i-1][j];
+        int take = INT_MAX;
+        if(coins[i]<=j) take=1+dp[i][j-coins[i]];
+        dp[i][j] = min(nonTake,take);
+            }
+        }
+        int ans= dp[index-1][amount];
         if(ans>=1e9) return -1;
         return ans;
     }
