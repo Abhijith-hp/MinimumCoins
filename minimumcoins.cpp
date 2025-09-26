40edde88318a440f54e3e6f4b56e8c8b1de0cbed
abhijith_g1 <abhijith_g@geojit.com>
2025-09-26 17:57:40 +0530

    Implemented Mim=nimum coins problem using tabulation under space optmization

--- minimumcoins.cpp
+++ minimumcoins.cpp
@@ -3,18 +3,21 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int index = coins.size();
         vector<vector<int>> dp(index, vector<int>(amount + 1, 1e9));
+        vector<int>prev(amount+1,1e9);
+        vector<int>curr(amount+1,1e9);
         for(int k=0;k<=amount;k++) {
-            if(k%coins[0]==0) dp[0][k] = k/coins[0];
+            if(k%coins[0]==0) prev[k] = k/coins[0];
         }
         for(int i=1;i<index;i++){
             for(int j=0;j<=amount;j++){
-                int nonTake = dp[i-1][j];
+                int nonTake = prev[j];
         int take = INT_MAX;
-        if(coins[i]<=j) take=1+dp[i][j-coins[i]];
-        dp[i][j] = min(nonTake,take);
+        if(coins[i]<=j) take=1+curr[j-coins[i]];
+        curr[j] = min(nonTake,take);
             }
+            prev=curr;
         }
-        int ans= dp[index-1][amount];
+        int ans= prev[amount];
         if(ans>=1e9) return -1;
         return ans;
     }
